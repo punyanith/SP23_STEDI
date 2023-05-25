d5a8e7732cfb2a7f33e86cfe06b4faaa88227ea8
Pun <punyanith.t@gmail.com>
2023-05-25 10:55:55 -0600

    fixed

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.get("/pun", (req, res)=>{
+
+    res.send("hello");
+})
+
 app.get("/", (req, res)=>{
 
     res.send("welcome to my backend API");
