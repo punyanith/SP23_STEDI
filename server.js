276e9fd51aedd723eaafc2766ff1eb96b39d7677
Pun <punyanith.t@gmail.com>
2023-05-25 10:32:50 -0600

    add

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get("/", (req, res)=>{
+
+    res.send("welcome to my backend API");
+
+})
+
+app.listen(3000, ()=> {
+
+    console.log('listening');
+})
